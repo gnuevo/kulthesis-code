1964cebf3053c8f4e4a2d8522e5369ffe9496525
Gregorio Nuevo <gncdvp@gmail.com>
2017-07-15 17:57:06 +0200

    First generator for autoencoder

--- generator/SimpleAutoencoderGenerator.py
+++ generator/SimpleAutoencoderGenerator.py
@@ -1,20 +1,32 @@
-"""Formats and augmentates the data to be fed to a simple autoencoder (input = target)
+"""Simple generator for an autoencoder
 
+This is the first generator programmed. It's very simple. In a nutshell it's
+just a wrapper over some functions in generatorutils
 """
+import h5py
+import generatorutils as genutils
 
 class SimpleAutoencoderGenerator(object):
     """Reads data from hdf5, formats it and augmentates it
     
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, hdf5_file, hdf5_group, buffer_size=1000000):
+        self.h5f = h5py.File(hdf5_file, 'r')
+        self.h5g = self.h5f[hdf5_group]
+        self.buffer_size = buffer_size
 
-    def __iter__(self):
-        pass
-
-    def next(self):
-        """Guarantees compatibility with 2.x
-        
-        :return: nex
-        """
+    def generate_samples(self, sample_length, step=None):
+        if step == None: step = sample_length
+        try:
+            data = self.h5g['data']  # take dataset
+            metadata = self.h5g.attrs['metadata']
+            chunk_size = metadata['chunk_size']
+            nsongs = metadata['Nsongs']
+        except:
+            print("Error getting data. Some of the needed fields do not exist")
+            raise
+        for sample in genutils.read_samples(data, sample_length, chunk_size,
+                                            step=step,
+                                            buffer_size=self.buffer_size):
+            yield sample, sample
