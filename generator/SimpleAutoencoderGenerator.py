a378b8631ba770c4d12280cc42a03bedfabf03dd
Gregorio Nuevo <gncdvp@gmail.com>
2017-07-15 17:24:17 +0200

    Starting with a simple generator for the autoencoder

---
+++ generator/SimpleAutoencoderGenerator.py
@@ -0,0 +1,20 @@
+"""Formats and augmentates the data to be fed to a simple autoencoder (input = target)
+
+"""
+
+class SimpleAutoencoderGenerator(object):
+    """Reads data from hdf5, formats it and augmentates it
+    
+    """
+
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def next(self):
+        """Guarantees compatibility with 2.x
+        
+        :return: nex
+        """
