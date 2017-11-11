bedb8ec6d7c63c7051ec9f1f22078278704ac3f4
Gregorio Nuevo <gncdvp@gmail.com>
2017-11-11 15:46:12 +0900

    Added support for TensorBoard and validation code

--- models/simple_autoencoder_with_generator.py
+++ models/simple_autoencoder_with_generator.py
@@ -10,6 +10,7 @@ interesting things:
 """
 from keras.layers import Input, Dense, Reshape
 from keras.models import Model
+from keras.callbacks import TensorBoard
 from generator.SimpleAutoencoderGenerator import SimpleAutoencoderGenerator
 from functools import reduce
 import operator
@@ -30,9 +31,13 @@ class AutoencoderWithGenerator(object):
                 network
             encoding_dim (int): size of the encoded layer of the autoencoder 
         """
+        self.__hdf5_file = hdf5_file
+        self.__hdf5_group = hdf5_group
         self.__generator = SimpleAutoencoderGenerator(hdf5_file, hdf5_group)
         self.__input_dimension = input_dimension # equivalent to sample_length
         self.__encoding_dim = encoding_dim
+        # list to store the callbacks, initialised empty
+        self.__callbacks = []
 
         self.__initialise(input_dimension=input_dimension, encoding_dim=encoding_dim)
 
@@ -53,7 +58,7 @@ class AutoencoderWithGenerator(object):
         
         Args:
             input_dimension (int or tuple of ints): input dimension for the 
-                network, if it is a touple the input is flattened to only 
+                network, if it is a tuple the input is flattened to only 
                 one dimension
             encoding_dim (int): compressed size
         """
@@ -70,41 +75,78 @@ class AutoencoderWithGenerator(object):
         # reshape if necessary, encode
         if len(input_dimension) > 1:
             input_reshape = Reshape(flattened_dimension,
-                                    input_shape=input_dimension)(
+                                    input_shape=input_dimension,
+                                    name="input_reshape")(
                 input_tensor)
-            encoded = Dense(encoding_dim, activation='relu')(input_reshape)
+            encoded = Dense(encoding_dim, activation='relu',
+                            name="encoder")(input_reshape)
         else:
-            encoded = Dense(encoding_dim, activation='relu')(input_tensor)
+            encoded = Dense(encoding_dim, activation='relu', name="encoder")(
+                input_tensor)
 
         # decode, reshape if necessary
         if len(input_dimension) > 1:
-            decoded = Dense(flattened_dimension[0], activation='relu')(encoded)
+            decoded = Dense(flattened_dimension[0], activation='relu',
+                            name="decoder"
+                            )(encoded)
             output = Reshape(input_dimension,
-                             input_shape=flattened_dimension)(decoded)
+                             input_shape=flattened_dimension,
+                             name="output_reshape")(
+                decoded)
         else:
-            decoded = Dense(flattened_dimension, activation='relu')(encoded)
+            decoded = Dense(flattened_dimension, activation='relu',
+                            name="decoder")(encoded)
             output = decoded
 
         # this model maps an input to its reconstruction
         self.__autoencoder = Model(input_tensor, output)
         self.__autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
+    def callback_add_tensorboard(self, log_dir='/tmp/autoencoder',
+                                 histogram_freq=1, write_graph=True):
+        tensorboard_callback = TensorBoard(log_dir=log_dir,
+                                           histogram_freq=histogram_freq,
+                                           write_graph=write_graph)
+        # remove a posible tensorboard callback from the list
+        # there is no point into keeping several callbacks to tensorflow
+        self.__callbacks = [callback for callback in self.__callbacks if not
+                        type(callback) == type(tensorboard_callback)]
+        self.__callbacks.append(tensorboard_callback)
+
     def fit_generator(self, batch_size=1000, epochs=1, step=None,
-                      section=None):
+                      train_section=None, val_section=None):
         if step == None: step = self.__input_dimension[0]
+        if val_section:
+            val_generator = SimpleAutoencoderGenerator(self.__hdf5_file,
+                                                       self.__hdf5_group)
+            val_generator.configure(sample_length=self.__input_dimension[0],
+                                   batch_size=batch_size,
+                                   step=step,
+                                   section=val_section)
+            val_data = val_generator.generate_samples()
+            val_steps = val_generator.get_nbatches_in_epoch()
+        else:
+            val_data = None
+            val_steps = None
+
+        callbacks = self.__callbacks if not self.__callbacks == [] else None
 
         self.__generator.configure(sample_length=self.__input_dimension[0],
                                    batch_size=batch_size,
                                    step=step,
-                                   section=section)
+                                   section=train_section)
         print(self.__generator.get_nbatches_in_epoch())
         self.__autoencoder.fit_generator(
             self.__generator.generate_samples(),
             self.__generator.get_nbatches_in_epoch(),
-            epochs=epochs
+            validation_data=val_data,
+            validation_steps=val_steps,
+            epochs=epochs,
+            callbacks=callbacks
             )
 
-    def train_dataset(self, batch_size=1000, epochs=1, step=None):
+    def train_dataset(self, batch_size=1000, epochs=1, step=None,
+                      validation=True):
         """Trains the dataset using the train section
         
         Gets the train section from the dataset and trains it with that section
@@ -121,9 +163,15 @@ class AutoencoderWithGenerator(object):
         train_section = data.attrs["train_set"]
         train_section = tuple(train_section)
 
+        if validation:
+            val_section = data.attrs["val_set"]
+            val_section = tuple(val_section)
+        else:
+            val_section = None
+
         # train
         self.fit_generator(batch_size=batch_size, epochs=epochs, step=step,
-                           section=train_section)
+                           train_section=train_section, val_section=val_section)
 
     def validate_dataset(self):
         pass
--- run_scripts/test_awg.py
+++ run_scripts/test_awg.py
@@ -25,6 +25,10 @@ if __name__ == "__main__":
                         default=None)
     parser.add_argument("-e", "--epochs", help="The number of epochs to "
                                                "compute", type=int, default=1)
+    parser.add_argument("--validate", help="Performs validation after each "
+                                           "epoch", action="store_true")
+    parser.add_argument("--tblogdir", help="logdir of tensorboard",
+                        type=str, default="/tmp/tensorboard")
     dargs = parser.parse_args()
 
     hdf5_file = dargs.hdf5_file
@@ -34,10 +38,14 @@ if __name__ == "__main__":
     batch_size = dargs.batch
     step = dargs.step
     epochs = dargs.epochs
+    log_dir = dargs.tblogdir
+    validation = dargs.validate
 
     autoencoder = AutoencoderWithGenerator(hdf5_file, hdf5_group,
                                            input_dimension=(dimension, 2),
                                            encoding_dim=encoded_size)
     print(autoencoder)
     # autoencoder.fit_generator(batch_size=batch_size, step=step, epochs=2)
-    autoencoder.train_dataset(batch_size=batch_size, step=step, epochs=epochs)
+    autoencoder.callback_add_tensorboard(log_dir=log_dir)
+    autoencoder.train_dataset(batch_size=batch_size, step=step,
+                              epochs=epochs, validation=validation)
