80b03b9ac9b0942861545b3bac5f61d3cd8f59fe
Gregorio Nuevo <gncdvp@gmail.com>
2017-12-09 14:19:06 +0900

    Simple reader

---
+++ generator/readers.py
@@ -0,0 +1,149 @@
+"""This file is intended to store readers
+
+Readers are code that read samples from the dataset
+"""
+import h5py
+import numpy as np
+
+
+def flatten_shape(array):
+    """Returns the new shape of the array if the first dimension is flatten
+
+    This is basically a method that returns the shape of the array with the 
+    two first dimensions multiplied (and thus merged as the new first 
+    dimension)
+
+    Args:
+        array (np.ndarray): array to be flatten
+
+    Returns (tuple): new shape
+    """
+    if len(array.shape) < 2:
+        raise ValueError("array variable must have at least 2 dimensions")
+    new_dim = array.shape[0] * array.shape[1]
+    if len(array.shape) == 2:
+        new_shape = (new_dim,)
+    elif len(array.shape) > 2:
+        new_shape = [new_dim]
+        new_shape.extend(array.shape[2:])
+        new_shape = tuple(new_shape)
+    return new_shape
+
+
+def Dataset(file, group):
+    """Takes file and group and returns the corresponding dataset
+    
+    Args:
+        file: name of the hdf5 file
+        group: group inside the hdf5 file
+
+    Returns: the dataset
+
+    """
+    h5f = h5py.File(file, 'r')
+    h5g = h5f[group]
+    dataset = h5g['data']
+    return dataset
+
+
+class Reader(object):
+    """Creates the samples reading the input dataset (np.array)
+
+        This procedure includes online processing (chunks of the dataset are 
+        read while execution and they are formated into the desired length); and 
+        data augmentation (using the step argument we can augmentate the number 
+        of samples generated).
+
+        It is worth to notice these characteristics:
+        1. The dataset is processed as a whole. No distinction between songs is 
+        made. Therefore samples from one song can be mixed with samples from the
+        previous or next song.
+        2. The step procedure doesn't add artificial 0's at the end in case that is
+        necessary to complete a sample. Therefore, the last audio samples of the
+        dataset may be ignored.
+        """
+    def __init__(self, dataset, sample_length, chunk_size, step=None,
+                 section=None, buffer_size=100000):
+        """Initialises the object and sets the necessary values
+        
+        Args:
+            dataset: hdf5 dataset or np.array containing the audio samples. It 
+            is assumed that it is divided into chunks
+            sample_length (int): desired output length of the samples
+            chunk_size (int): size of the chunks in the dataset
+            section (tuple): indicates the section intended to read inside the 
+                dataset. E.g. if section=(0,100) then the function reads 
+                elements from 0 to 100 of the dataset (both included). If None 
+                then all the dataset is read.
+            step (int): (optional) step between samples 
+            buffer_size (int): (optional) number of samples that are read to 
+                memory at once
+            section (int, int): (tuple) first and last index of the section of 
+                the dataset that is intended to read. The dataset is virtually 
+                limited to the section from first index to last index
+        """
+        self.dataset = dataset
+        self.sample_length = sample_length
+        self.chunk_size = chunk_size
+        self.step = step
+        self.section = section
+        self.buffer_size = buffer_size
+
+    def read_samples(self):
+        """Generates samples following the specifications
+
+            Returns:
+                a generator that generates sample after sample following the 
+                specifications until the end of the dataset is reached.
+        """
+        # extract object variables to local variables
+        if self.section == None or not type(self.section) == tuple:
+            section = (0, len(self.dataset))
+        start_of_section = section[0]
+        end_of_section = section[1]
+
+        dataset = self.dataset
+        buffer_size = self.buffer_size
+        chunk_size = self.chunk_size
+        sample_length = self.sample_length
+        step = self.step
+        if step == None: step = sample_length
+
+        # by the extra substraction we protect ourselves from overflowing the
+        # buffer
+        # TODO this may not be the best way, but if there are remaining samples
+        # from the previoius iteration and we load to memory more from disk we
+        # may overflow the buffer
+        chunks_in_buffer = buffer_size // chunk_size - chunk_size // \
+                                                       sample_length - 1
+
+
+        buffer = dataset[start_of_section]  # load first chunk
+        bottom_index = start_of_section + 1  # +1 because the first chunk (
+        # start_of_section) was read already
+        top_index = bottom_index  # just some initialisation
+
+        step_index = 0  # index to move step by step
+
+        # loop
+        while top_index < end_of_section:
+            if bottom_index + chunks_in_buffer < end_of_section:
+                top_index = bottom_index + chunks_in_buffer
+            else:
+                top_index = end_of_section
+
+            new_read_data = dataset[bottom_index:top_index]
+            bottom_index = top_index
+            new_read_data = np.reshape(new_read_data,
+                                       flatten_shape(new_read_data))
+            buffer = np.concatenate((buffer[step_index:], new_read_data),
+                                    axis=0)
+            step_index = 0
+
+            for index in range(0, buffer.shape[0] - sample_length, step):
+                # iterate over all the samples taken from the buffer
+                yield buffer[index:index + sample_length]
+
+            # prepare old samples to disposal, by doing this, all the samples
+            # 'begind' have been used already
+            step_index = index + step
