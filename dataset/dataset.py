556f309fff47227553a9e6933fc01378f1a924e3
Gregorio Nuevo <gncdvp@gmail.com>
2018-01-20 12:03:51 +0900

    datasef file now contains DatassetSection object

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -4,6 +4,8 @@
 import h5py
 import math
 import json
+from collections import Sequence
+from generator.generatorutils import song_section_to_chunk_section
 
 
 class DatasetFormatError(Exception):
@@ -87,3 +89,75 @@ class Group(object):
                               batch_size)
         print("Total number of batches", n_batches)
         return n_batches
+
+
+class DatasetSection(Sequence):
+    """Encapsulates a section of the dataset with a Sequence easy access API
+    
+    The idea behind this is that the DataSection object reads chunks at a 
+    low level, allowing to perform some processing to data that would be 
+    considered as preprocessing for the network. For example, selecting only
+    one channel of audio, normalising the dataset, etc. Therefore, 
+    the Reader only cares about doing processing to the signal related to 
+    training, for example adding noise to challenge the autoencoder.
+    
+    """
+
+    def __init__(self, group_data, section=None, section_format="songs",
+                 stereo=False,
+                 channel=0):
+        """
+        
+        Args:
+            group_data: Group.data()
+            section: tuple with the boundaries of the section
+            section_format: format of the section ('songs' or 'chunks')
+            stereo: if you want to treat data as stereo or mono
+            channel: channel you want to get if stereo=False and data is stereo
+        """
+        self._group_data = group_data
+        if section == None:
+            self._start_section = 0
+            self._stop_section = group_data # FIXME, I think this is
+            # wrong, len() - 1?
+        else:
+            if section_format == "songs":
+                chunk_section = song_section_to_chunk_section(group_data,
+                                                              song_section=section)
+                self._start_section = chunk_section[0]
+                self._stop_section = chunk_section[1]
+            elif section_format == "chunks":
+                self._start_section = section[0]
+                self._stop_section = section[1]
+            else:
+                raise ValueError("section_format must be either 'songs' or "
+                                 "'chunks', gotten", section_format)
+
+        # create a filter function to extract the desired channel(s)
+        if stereo == True:
+            self._filter = lambda x: x
+        elif stereo == False:
+            if type(channel) == int and channel < self._group_data.shape[-1]:
+                # return the selected channel
+                self._filter = lambda x: x[:, channel]
+            else:
+                raise ValueError("channel is not <int> or is out of range, "
+                                 "channel=", channel)
+
+    def __len__(self):
+        # FIXME, I'm not sure this is the correct function
+        return self._stop_section - self._start_section
+
+    def __getitem__(self, item):
+        # in order to retrieve data we have to index over the section by
+        # adding self._start_section to our indices
+        if type(item) == int:
+            item += self._start_section
+        elif type(item) == slice:
+            item.start += self._start_section
+            item.stop += self._start_section
+        else:
+            raise TypeError("Item type is not <int> nor <slice>, type",
+                            type(item))
+        retrieved = self._group_data[item]
+        return self._filter(retrieved)
