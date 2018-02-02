b5c9aa6089ebb1d3764c4caac73ce844c7c10e3a
Gregorio Nuevo <gncdvp@gmail.com>
2018-02-02 21:35:32 +0900

    Improved checkpoint callback

--- callbacks/customCheckPoint.py
+++ callbacks/customCheckPoint.py
@@ -10,10 +10,17 @@ class customModelCheckpoint(kcallbacks.Callback):
         self._period = period
         self._epochs_since_last_save = 0
 
+    def _save_model(self, epoch, logs=None):
+        self._model.save(self._dirpath, {"last_epoch": epoch})
+
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
+        self._last_epoch = epoch
         self._epochs_since_last_save += 1
         if self._epochs_since_last_save >= self._period:
             self._epochs_since_last_save = 0
             print("saving model")
-            self._model.save(self._dirpath, {"last_epoch": epoch})
+            self._save_model(epoch, logs)
+
+    def on_train_end(self, logs=None):
+        self._save_model(self._last_epoch, logs)
